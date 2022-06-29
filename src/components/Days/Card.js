908f026d8ed738d223a813f2b3124947771173f6
Baubek Mazhitbekov <baukenzo16@gmail.com>
2022-06-29 15:21:43 +0600

    bbbb

---
+++ src/components/Days/Card.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { GlobalSvgSelector } from '../../assets/icons/global/GlobalSvgSelector';
+
+import s from './Days.module.scss';
+
+
+
+export const Card = ({ day }) => {
+  return (
+    <div className={s.card}>
+      <div className={s.day}>{day.day}</div>
+      <div className={s.day__info}>{day.day_info}</div>
+      <div className={s.img}>
+        <GlobalSvgSelector id={day.icon_id} />
+      </div>
+      <div className={s.temp__day}>{day.temp_day}</div>
+      <div className={s.temp__night}>{day.temp_day}</div>
+      <div className={s.info}>{day.info}</div>
+    </div>
+  );
+};
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,21 +1,16 @@
 import React from 'react';
+import { Days } from '../components/Days/Days';
 import Searchbar from '../components/Searchbar/Searchbar';
-<<<<<<< HEAD
 import ThisDay from '../components/ThisDay/ThisDay';
 // import { ThisDayInfo } from '../components/ThisDayInfo/ThisDayInfo';
-=======
 import { ThisDayInfo } from '../components/ThisDayInfo/ThisDayInfo';
->>>>>>> 9668f1ee56a46e64232eda83422ac9f5f5909a27
 
 const HomePage = () => {
     return (
         <div>
             <Searchbar/>
-<<<<<<< HEAD
             <ThisDay/>
-=======
             <ThisDayInfo/>
->>>>>>> 9668f1ee56a46e64232eda83422ac9f5f5909a27
         </div>
 
     );
