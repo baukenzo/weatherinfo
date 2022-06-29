55b434a9d1a3a2c297f37cebce27b4cb62499c49
Baubek Mazhitbekov <baukenzo16@gmail.com>
2022-06-29 14:12:09 +0600

    scss

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import Searchbar from '../components/Searchbar/Searchbar';
-import ThisDay from '../components/ThisDay/Thisday';
-import { ThisDayInfo } from '../components/ThisDayInfo/ThisDayInfo';
 
 const HomePage = () => {
     return (
         <div>
             <Searchbar/>
-            <ThisDay/>
-            <ThisDayInfo/>
         </div>
 
     );
