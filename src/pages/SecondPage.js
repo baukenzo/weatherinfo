0c1ddacd461e6690285578edffb633ea70c5cd04
Baubek Mazhitbekov <baukenzo16@gmail.com>
2022-07-04 19:19:30 +0600

    add some changes

--- src/pages/SecondPage.js
+++ src/pages/SecondPage.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import {motion} from 'framer-motion';
+import add from '../images/add.jpg';
 
 const SecondPage = () => {
     return (
-        <motion.h1 initial={{width: 0, height: 0}} animate={{width: '100%'}} exit={{x: window.innerWidth}}>
-            Second Page
-        </motion.h1>
+        <motion.div  initial={{width: 0, height: 0}} animate={{width: '100%'}} exit={{x: window.innerWidth}}>
+            <img style={{width: '100%'}}  src={add} alt="" />
+        </motion.div>
     );
 };
 
